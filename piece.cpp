a59672f6d8689d376c416e59244f336f2fd10e21
tylermcm <tyllew96@gmail.com>
2023-10-13 02:49:48 -0600

    updated piece.cpp/h

--- piece.cpp
+++ piece.cpp
@@ -9,71 +9,107 @@
  *    chess pieces, including rules for their movement.
  ************************************************************************/
 
+#include <cassert>
+#include <iostream>
+
 #include "Piece.h"
 #include "board.h"
 
 
-Piece::Piece(int r, int c, bool white) : position(r, c), fWhite(white), nMoves(0), lastMove(-1) {}
-
-std::vector<Move> Piece::getMovesSlide(const Board& board, const std::vector<Position>& delta) {
-   std::vector<Move> moves;
-   for (const auto& d : delta) {
-      Position posMove = position + d;
-      Move move;
-      while (posMove.isValid() && board.get(posMove).getLetter() == ' ') 
-      { 
-         move.setSrc(getPosition());
-         move.setDes(posMove);
-         move.setWhiteMove(isWhite());
-         moves.push_back(move);
-         posMove += d;
-      }
-      if (posMove.isValid() && (board.get(posMove).isWhite() != isWhite() || board.get(posMove).getLetter() == ' ')) {
-         move.setSrc(getPosition());
-         move.setDes(posMove);
-         move.setWhiteMove(isWhite());
-         if (board.get(posMove).getLetter() != ' ') {
-            move.setCapture(board.get(posMove).getLetter());
-         }
-         moves.push_back(move);
-      }
-   }
-   return moves;
-}
-// Assign a new position to the piece
-void Piece::assign(Position newPosition) {
-   position = newPosition;
+void Space::getMoves(set<Move>& moves, const Board& board) const
+{
+	std::cout << "this working?" << std::endl;
 }
 
-// Assign properties from another piece
-void Piece::assign(Piece& piece) {
-   position = piece.getPosition();
-   fWhite = piece.isWhite();
-   nMoves = piece.getNMoves();
-   lastMove = piece.justMoved() ? piece.getNMoves() : -1; 
+
+void Pawn::addPromotion(set<Move>& moves, Move& move) const
+{
+	move.setPromote(QUEEN);
+	moves.insert(move);
 }
 
-// Check if the piece is white
-bool Piece::isWhite() const {
-   return fWhite;
+void Pawn::getMoves(set<Move>& moves, const Board& board) const
+{
+	{
+		Position posMove(getPosition(), isWhite() ? ADD_R : SUB_R);
+
+		if (posMove.isValid() && board[posMove] == ' ')
+		{
+			Move move;
+			move.setSrc(getPosition());
+			move.setDes(posMove);
+			move.setWhiteMove(isWhite());
+
+			if (posMove.getRow() == (isWhite() ? 7 : 0))
+				addPromotion(moves, move);
+			else
+				moves.insert(move);
+		}
+
+	}
+
+	if (!isMoved())
+	{
+		assert(position.getRow() == (isWhite() ? 1 : 6));
+		Position posMove(isWhite() ? 3 : 4, getPosition().getCol());
+		Position posCheck(isWhite() ? 2 : 5, getPosition().getCol());
+
+		if (board[posMove] == ' ' && board[posCheck] == ' ')
+		{
+			Move move;
+			move.setSrc(getPosition());
+			move.setDes(posMove);
+			move.setWhiteMove(isWhite());
+			moves.insert(move);
+		}
+	}
+
+	const int cDelta[] = { 1, -1 };
+	for (int i = 0; i < 2; i++)
+	{
+		Position posMove(position.getRow() + (isWhite() ? 1 : -1),
+			position.getCol() + cDelta[i]);
+		if (posMove.isValid() && 
+			board[posMove] != ' ' && 
+			board[posMove].isWhite() != isWhite())
+		{
+			Move move;
+			move.setSrc(getPosition());
+			move.setDes(posMove);
+			move.setWhiteMove(isWhite());
+			move.setCapture(board[posMove].getLetter());
+
+			if (posMove.getRow() == (isWhite() ? 7 : 0))
+				addPromotion(moves, move);
+			else
+				moves.insert(move);
+		}
+	}
+
+	//en passant
+	for (int i = 0; i < 2; i++)
+	{
+		Position posMove(position.getRow() + (isWhite() ? 1 : -1),
+			position.getCol() + cDelta[i]);
+		Position posKill(position.getRow(),
+			position.getCol() + cDelta[i]);
+
+		if (posMove.isValid() &&
+			position.getRow() == (isWhite() ? 4 : 3) &&
+			board[posMove] == ' ' &&
+			board[posKill] == 'p' &&
+			board[posKill].isWhite() != isWhite() &&
+			board[posKill].getNMoves() == 1 &&
+			board[posKill].justMoved(board.getCurrentMove()))
+		{
+			Move move;
+			move.setSrc(getPosition());
+			move.setDes(posMove);
+			move.setWhiteMove(isWhite());
+			move.setCapture(board[posMove].getLetter());
+			move.setEnPassant();
+			moves.insert(move);
+		}
+	}
 }
 
-// Check if the piece has moved
-bool Piece::isMove() const {
-   return nMoves > 0;
-}
-
-// Get the number of moves made by this piece
-int Piece::getNMoves() const {
-   return nMoves;
-}
-
-// Get the current position of the piece
-Position Piece::getPosition() const {
-   return position;
-}
-
-// Check if the piece just moved
-bool Piece::justMoved() const {
-   return lastMove == nMoves;
-}
--- piece.h
+++ piece.h
@@ -10,39 +10,6 @@
  ************************************************************************/
 
 #pragma once
-//
-//#include "Position.h"  // Assuming Position is defined in a separate header file
-//#include "Board.h"  // Assuming Board is defined in a separate header file
-//#include "uiDraw.h"  // Assuming ogstream is a type you've defined or imported
-//#include <vector>  // For the getMoves method
-//
-//class Piece {
-//protected:
-//   Position position;  // The position of the piece on the board
-//   bool fWhite;  // Whether the piece is white or not
-//   int nMoves;  // Number of moves made by this piece
-//   int lastMove;  // The last move number on which this piece was moved
-//
-//public:
-//   // Constructors
-//   Piece(int r, int c, bool white);
-//
-//   // Public Methods
-//   void assign(Position position);  // Assign a new position to the piece
-//   void assign(Piece& piece);  // Assign properties from another piece
-//   bool isWhite() const;  // Check if the piece is white
-//   bool isMove() const;  // Check if the piece has moved
-//   int getNMoves() const;  // Get the number of moves made by this piece
-//   Position getPosition() const;  // Get the current position of the piece
-//   bool justMoved() const;  // Check if the piece just moved
-//
-//   // Virtual Methods
-//   virtual char getLetter() const = 0;  // Get the letter representation of the piece
-//   virtual void display(ogstream& gout) = 0;  // Display the piece
-//   virtual std::vector<Position> getMoves(const Board& board) = 0;  // Get possible moves
-//   virtual PieceType getPieceType() const = 0;
-//};
-
 #include <set>
 #include "position.h"
 #include "move.h"
